3219b34ae715031a6bdfb1a95fa3832006a076a9
oleksandr <oleksandr.bevzyuk@gmail.com>
2015-06-24 12:52:52 +0300

    Add URL string
    Separate URL string

--- Weather/src/com/bevziuk/Weather/HttpActivity.java
+++ Weather/src/com/bevziuk/Weather/HttpActivity.java
@@ -5,6 +5,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 
 import java.util.List;
+import java.util.PriorityQueue;
 
 
 public class HttpActivity extends ListActivity {
@@ -17,6 +18,17 @@ public class HttpActivity extends ListActivity {
     private class HttpGetTask extends AsyncTask<Void, Void, List<String>> {
 
         private static final String URL = "http://api.openweathermap.org/data/2.5/forecast/daily?id=703448&mod=json&snt=7&unit=metric&lang=ua";
+
+        private final Integer ID = 703448; //Kiev
+
+        private final String MOD = "json";
+
+        private final Integer DAY = 7; //7 days
+
+        private final String METRIC = "metric";
+
+        private final String LANG = "ua";
+
     }
 }
 
