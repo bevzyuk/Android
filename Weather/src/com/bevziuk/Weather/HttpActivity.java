ccfdc171010f07528b55b699869d7641eeffe60d
oleksandr <oleksandr.bevzyuk@gmail.com>
2015-06-24 13:20:15 +0300

    Add URL string
    Separate URL string

--- Weather/src/com/bevziuk/Weather/HttpActivity.java
+++ Weather/src/com/bevziuk/Weather/HttpActivity.java
@@ -1,11 +1,21 @@
 package com.bevziuk.Weather;
 
 import android.app.ListActivity;
+import android.net.http.AndroidHttpClient;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import org.apache.http.client.methods.HttpGet;
 
 import java.util.List;
-import java.util.PriorityQueue;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
 
 public class HttpActivity extends ListActivity {
@@ -17,18 +27,30 @@ public class HttpActivity extends ListActivity {
 
     private class HttpGetTask extends AsyncTask<Void, Void, List<String>> {
 
-        private static final String URL = "http://api.openweathermap.org/data/2.5/forecast/daily?id=703448&mod=json&snt=7&unit=metric&lang=ua";
-
         private final Integer ID = 703448; //Kiev
-
         private final String MOD = "json";
-
         private final Integer DAY = 7; //7 days
-
         private final String METRIC = "metric";
-
         private final String LANG = "ua";
+        private  final String URL = "http://api.openweathermap.org/data/2.5/forecast/daily?id="
+                +ID+"&mod="+MOD+"&snt="+DAY+"&unit="+METRIC+"&lang="+LANG;
 
+        AndroidHttpClient mClient = AndroidHttpClient.newInstance("");
+
+        @Override
+        protected List<String> doInBackground(Void... params) {
+
+            HttpGet request = new HttpGet(URL);
+
+            try {
+                mClient.execute();
+            }
+
+
+
+          //  JSONResponseHandler responseHandler = new JSONResponseHandler();
+            return null;
+        }
     }
 }
 
